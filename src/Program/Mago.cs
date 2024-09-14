a7ef016c34ba9c97d7608f1b292d66381830f1f6
Your Name <you@example.com>
2024-09-13 22:43:58 -0300

    Mago

--- src/Program/Mago.cs
+++ src/Program/Mago.cs
@@ -1,6 +1,63 @@
-namespace Program;
-
 public class Mago
 {
+    public string Nombre { get; set; }
+    public int Vida { get; set; }
+    public int Ataque { get; set; }
+    public int Defensa { get; set; }
     
+    public Arco Arco { get; private set; }
+    public Espada Espada { get; private set; }
+    public Escudo Escudo { get; private set; }
+    public ArrayList Items { get; set; }
+    
+    public Mago(string nombre, int vida, int ataque, int defensa)
+    {
+        Nombre = nombre;
+        Vida = vida;
+        Ataque = ataque;
+        Defensa = defensa;
+        Items = new ArrayList { arco, espada, escudo };
+    }
+    
+    public int GetVida()
+    {
+        return Vida; 
+    }
+    
+    public void AgregarItems(Arco arco, Espada espada, Escudo escudo)
+    {
+        Items.Add(arco);
+        Items.Add(espada);
+        Items.Add(escudo);
+    }
+   
+    public int GetAtaquefinal()
+    {
+        int ataqueTotal = Ataque;
+        foreach (var item in Items)
+        {
+            if (item is Arco arco)
+            {
+                ataqueTotal += arco.Ataque;
+            }
+            else if (item is Espada espada)
+            {
+                ataqueTotal += espada.Ataque;
+            }
+        }
+        return ataqueTotal;
+    }
+    
+    public int GetDefensfinal()
+    {
+        int defensaTotal = Defensa;
+        foreach (var item in Items)
+        {
+            if (item is Escudo escudo)
+            {
+                defensaTotal += escudo.Defensa;
+            }
+        }
+        return defensaTotal;
+    }
 }
